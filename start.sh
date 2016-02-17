94748a0db6647367858c0de038553e8949f30b37
wiber <elias@youiest.com>
2016-02-17 09:47:31 +0100

    fewer comments

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 # this script shows last commits on the repo,
-# checks out last branch worked on,
 # and starts demo and package tests
 for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\t$branch; done | sort -r
 git log --branches -1 --pretty=format:'%D'
-# To go to the branch of the most recent commit:
-# http://stackoverflow.com/questions/2427288/how-to-get-back-to-the-latest-commit-after-checking-out-a-previous-commit
-#git checkout $(git log --branches -1 --pretty=format:'%D' |sed 's/HEAD -> \|tag: [^,]*\|,.*//g')
-# trap ctrl-c and call ctrl_c()
+
 trap ctrl_c INT
 
 function ctrl_c() {
