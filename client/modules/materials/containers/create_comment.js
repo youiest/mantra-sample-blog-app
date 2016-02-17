81512d0e99713fb764e4d22783dea96800f43b54
wiber <elias@youiest.com>
2016-02-17 15:13:31 +0100

    non core module

--- client/modules/materials/containers/create_comment.js
+++ client/modules/materials/containers/create_comment.js
@@ -1,22 +1,23 @@
-import NewPost from '../components/newpost.jsx';
-import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
+import {
+  useDeps, composeWithTracker, composeAll
+} from 'mantra-core';
+import Component from '../components/create_comment.jsx';
 
 export const composer = ({context, clearErrors}, onData) => {
   const {LocalState} = context();
-  const error = LocalState.get('SAVING_ERROR');
+  const error = LocalState.get('CREATE_COMMENT_ERROR');
   onData(null, {error});
 
-  // clearErrors when unmounting the component
   return clearErrors;
 };
 
 export const depsMapper = (context, actions) => ({
-  create: actions.posts.create,
-  clearErrors: actions.posts.clearErrors,
+  create: actions.comments.create,
+  clearErrors: actions.comments.clearErrors,
   context: () => context
 });
 
 export default composeAll(
   composeWithTracker(composer),
   useDeps(depsMapper)
-)(NewPost);
+)(Component);
